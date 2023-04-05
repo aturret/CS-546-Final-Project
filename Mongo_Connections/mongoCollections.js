c8565cafa6a4abdb13bff0a8f07ee73943e52e07
Jack Wang <wzh960319@gmail.com>
2023-04-05 17:53:29 -0400

    initial commit

---
+++ Mongo_Connections/mongoCollections.js
@@ -0,0 +1,19 @@
+import { dbConnection } from './mongoConnection.js.js';
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+// Note: You will need to change the code below to have the collection required by the assignment!
+export const account = getCollectionFn('accounts')
+export const order = getCollectionFn('orders')
+export const review = getCollectionFn('reviews')
