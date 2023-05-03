3425fc67a866b830d177704c279bff448f5cdc01
aturret <enturreopy@gmail.com>
2023-05-02 23:23:31 -0400

    Update validation.js

--- public/js/validation.js
+++ public/js/validation.js
@@ -12,7 +12,25 @@ forms.forEach(form => {
         const passwordInputs = document.querySelectorAll('input[type="password"]');
         const dateInputs = document.querySelectorAll('input[type="date"]');
         const emailInputs = document.querySelectorAll('input[type="email"]');
-        const phoneInputs = document.querySelectorAll('input[class="phone"]');    
+        const phoneInputs = document.querySelectorAll('input[class="phone"]');
+        try {
+            passwordInputs.forEach(input => {
+            })
+            dateInputs.forEach(input => {
+                input.value = checkDate(input.value, false);
+            })
+            emailInputs.forEach(input => {
+                input.value = checkEmail(input.value, false);
+            })
+            phoneInputs.forEach(input => {
+                input.value = checkPhone(input.value, false);
+            })
+        }
+        catch (e) {
+            e.preventDefault();
+            clientErrorDiv.style.display = 'block';
+            clientError.innerHTML = e.message;
+        }
     }
     )
 })
@@ -21,3 +39,39 @@ forms.forEach(form => {
 
 
 
+
+
+
+
+
+function CustomException(message, flag) {
+    const error = new Error(message);
+
+    error.code = flag ? 400 : 404;
+    return error;
+}
+
+
+function checkDate(date, flag) {
+    if (!date || typeof date !== "string" || date.trim().length === 0) throw new CustomException("releaseDate must exist and must be a non empty string.", flag);
+    if (!moment(date.trim(), "YYYY/MM/DD", true).isValid()) throw new CustomException("releaseDate must be a valid date.", flag);
+    date = date.trim();
+    let temp = +date.split("/")[-1];
+    if (temp < 1900 || temp > 2023)
+        throw new CustomException("The year of release of the album must be between 1900 and 2024.", flag);
+
+    return date
+}
+function checkEmail(email, flag) {
+    email = email.trim()
+    if (!email || typeof email !== 'string') throw new CustomException("Email must exist and must be a string type", flag);
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) throw new CustomException("Invalid email address", flag)
+    return email
+}
+function checkPhone(n, flag)
+{
+    n = n.trim()
+    if(!n || typeof n !== 'string') throw new CustomException("Phone number must exist and must be a string type.", flag);
+    if(!/^\d{3}[-]?\d{3}[-]?\d{4}$/.test(n)) throw new CustomException("Cell phone number has to match the format. xxxxxxxxxxx, xxx-xxx-xxxxx or (xxx)xxx-xxxxx", flag)
+    return n
+}
