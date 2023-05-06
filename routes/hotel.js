09a1d7ca4f534d9bd7a59f3595bb634d31c252ac
zainzlh <67248490+zainzlh@users.noreply.github.com>
2023-05-06 03:53:33 -0400

    hotel routes

--- routes/hotel.js
+++ routes/hotel.js
@@ -9,6 +9,35 @@ import isAuth from "./user.js";
 
 const router = express.Router();
 
+export const isAuth = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return res.redirect("/user/login");
+  }
+  next();
+};
+
+export const isMgr = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return res.redirect("/user/login");
+  }
+  if (req.user && req.user.identity !== 'mgr') {
+    req.flash("You are not allow to access this page")
+    return res.redirect("/user/dashboard");
+  }
+  next();
+};
+
+export const isAdmin = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return res.redirect("/user/login");
+  }
+  if (req.user && req.user.identity !== 'admin') {
+    req.flash("You are not allow to access this page")
+    return res.redirect("/user/dashboard");
+  }
+  next();
+};
+
 //TODO: Hotel searching main page
 router
   .route("/")
@@ -88,5 +117,140 @@ router
 //TODO: Room detail page
 router.route("/hotel/:hotel_name/:room_id").get((req, res) => {});
 
+router
+  .route("/hotel/:hotelId/hotelManagement")
+  .get(isAdmin, async (req, res) => {
+    try {
+      const hotelId = helper.checkId(req.params.hotelId, true);
+      const hotel = hotelFuncs.getHotel(hotelId);
+      res.render('hotel_management', {hotel: hotel});
+    } catch (e) {
+      req.session.status = e.code ? e.code : 500;
+      req.session.errorMessage = e.message;
+      const previousUrl = req.headers.referer || '/hotel/:hotelId/';
+      res.redirect(previousUrl);
+    }
+  })
+  .put(isAdmin, async (req, res) => {
+    try {
+      const hotelId = req.params.hotelId;
+      const hotelName = req.body.hotelName;
+      const hotelStreet = req.body.hotelStreet;
+      const hotelCity = req.body.hotelCity;
+      const hotelState = req.body.hotelState;
+      const hotelZipCode = req.body.hotelZipCode;
+      const hotelPhone = req.body.hotelPhone;
+      const hotelEmail = req.body.hotelEmail;
+      const hotelPicture = req.body.hotelPicture;
+      const hotelFacilities = req.body.hotelFacilities;
 
+      const result = await hotelFuncs.updateHotel(
+        hotelId,
+        hotelName,
+        hotelStreet,
+        hotelCity,
+        hotelState,
+        hotelZipCode,
+        hotelPhone,
+        hotelEmail,
+        hotelPicture,
+        // rooms,
+        hotelFacilities,
+        // manager,
+        // roomType,
+        // reviews
+      );
+      req.flash(result);
+      return res.redirect(200).redirect("/hotel/:hotelId/hotelManagement");
+    } catch (e) {
+      e.code = e.code ? e.code : 500;
+      req.session.errorMessage = e.message;
+      res.redirect("/hotel_management");
+    }
+  })
+  .delete(isAdmin, async (req, res) => {
+    try {
+      const hotelId = helper.checkId(req.params.hotelId, true);
+      const message = hotelFuncs.deleteHotel(hotelId);
+      req.flash(message);
+      return res.redirect(200).redirect("/hotel");
+    } catch (e) {
+      req.session.status = e.code ? e.code : 500;
+      req.session.errorMessage = e.message;
+      const previousUrl = req.headers.referer || '/hotel';
+      res.redirect(previousUrl);
+    }
+  })
+
+router
+  .route("/hotel/:hotelId/hotelManagement/room")
+  .get(isAdmin, async (req, res) => {
+    try {
+      const hotelId = helper.checkId(req.params.hotelId, true);
+      const hotel = hotelFuncs.getHotel(hotelId);
+      res.render('hotel_management', {hotel: hotel});
+    } catch (e) {
+      req.session.status = e.code ? e.code : 500;
+      req.session.errorMessage = e.message;
+      const previousUrl = req.headers.referer || '/hotel/:hotelId/';
+      res.redirect(previousUrl);
+    }
+  })
+  .post(isAdmin, async (req, res) => {
+    try {
+      const hotelId = helper.checkId(req.params.hotelId, true);
+      const hotel = hotelFuncs.getHotel(hotelId);
+      res.render('hotel_management', {hotel: hotel});
+    } catch (e) {
+      req.session.status = e.code ? e.code : 500;
+      req.session.errorMessage = e.message;
+      const previousUrl = req.headers.referer || '/hotel/:hotelId/';
+      res.redirect(previousUrl);
+    }
+  })
+
+router
+  .route("/hotel/:hotelId/hotelManagement/roomtype")
+  .get(isAdmin, async (req, res) => {
+    try {
+      const hotelId = helper.checkId(req.params.hotelId, true);
+      const hotel = hotelFuncs.getHotel(hotelId);
+      res.render('hotel_management', {hotel: hotel});
+    } catch (e) {
+      req.session.status = e.code ? e.code : 500;
+      req.session.errorMessage = e.message;
+      const previousUrl = req.headers.referer || '/hotel/:hotelId/';
+      res.redirect(previousUrl);
+    }
+  })
+
+  router
+  .route("/hotel/:hotelId/hotelManagement/manager")
+  .get(isAdmin, async (req, res) => {
+    try {
+      const hotelId = helper.checkId(req.params.hotelId, true);
+      const hotel = hotelFuncs.getHotel(hotelId);
+      res.render('hotel_management', {hotel: hotel});
+    } catch (e) {
+      req.session.status = e.code ? e.code : 500;
+      req.session.errorMessage = e.message;
+      const previousUrl = req.headers.referer || '/hotel/:hotelId/';
+      res.redirect(previousUrl);
+    }
+  })
+
+  router
+  .route("/hotel/:hotelId/hotelManagement/review")
+  .get(isAdmin, async (req, res) => {
+    try {
+      const hotelId = helper.checkId(req.params.hotelId, true);
+      const hotel = hotelFuncs.getHotel(hotelId);
+      res.render('hotel_management', {hotel: hotel});
+    } catch (e) {
+      req.session.status = e.code ? e.code : 500;
+      req.session.errorMessage = e.message;
+      const previousUrl = req.headers.referer || '/hotel/:hotelId/';
+      res.redirect(previousUrl);
+    }
+  })
 export default router;
