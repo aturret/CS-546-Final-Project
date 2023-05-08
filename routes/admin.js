817d5b36d659d797e595d9a04c2d7cc9b133129f
zainzlh <67248490+zainzlh@users.noreply.github.com>
2023-05-08 14:42:15 -0400

    admin roll back

--- routes/admin.js
+++ routes/admin.js
@@ -51,7 +51,7 @@ router
   .get(isAdmin, async (req, res) => {
     try {
       const requestId = helper.checkId(req.params.requestId, true);
-      const request = await adminFuncs.getReq(requestId);
+      const request = await adminFuncs.getReqById(requestId);
       res.render('requestById', { request: request,title:"Hotel Application Processing Panel" });
     } catch (e) {
       if (!e.code) {
@@ -85,74 +85,13 @@ router
 router
   .route('/accounts')
   .get(isAdmin, async (req, res) => {
-    try {
-      const username = helper.checkString(req.body.usernameInput, "username", true);
-      const user = await userFuncs.getUser(username)
-      res.render('adminAccounts', { user: user ,title:"User Management Panel"});
-    } catch (e) {
-      if (!e.code) {
-        req.session.status = 500;
-      } else {
-        req.session.status = e.code;
-      }
-      req.session.errorMessage = e.message;
-      return res.redirect("/admin/accounts");
-    }
+    res.render('adminAccounts', { title:"User Management Panel" });
   })
   .post(isAdmin, async (req, res) => {
     try {
-      username = helper.checkString(username, "username", true);
-      role = helper
-        .checkRole(role, "identity", true)
-        .toLowerCase();
-      if (["manager", "user", "admin"].every((obj) => obj !== role))
-        throw CustomException("Invalid identity.", true);
-        avatar = avatar
-        ? helper.checkWebsite(avatar, true)
-        : undefined;
-      firstName = helper.checkNameString(
-        firstName,
-        "first name",
-        true
-      );
-      lastName = helper.checkNameString(
-        lastName,
-        "last name",
-        true
-      );
-      phone = phone ? helper.checkPhone(phone, true) : undefined;
-      password = helper.checkPassword(password, true);
-      email = helper.checkEmail(email, true);
-      
-      const args = [
-        'user',
-        username,
-        avatar,
-        firstName,
-        lastName,
-        phone,
-        password,
-        email,
-        []
-      ]
-
-      const newUserMessage = await userFuncs.create(args);
-
-      // if (identity === 'manager') {
-      //   const hotel = {};
-      //   hotel.name = helper.checkString(req.body.nameInput, "hotel name", true);
-      //   hotel.street = helper.checkString(req.body.streetInput, "street", true);
-      //   hotel.city = helper.checkString(req.body.cityInput, "city", true);
-      //   hotel.state = helper.checkString(req.body.stateInput, "state", true);
-      //   hotel.zip_code = helper.checkZip(req.body.zip_codeInput, true);
-
-      //   const addMgrMessage = await adminFuncs.addMgr(req.user.username, username, hotel);
-      //   req.flash(addMgrMessage);
-      //   return res.redirect("/admin/account");
-      // }
-
-      req.flash(newUserMessage);
-      return res.redirect("/admin/accounts");
+      const searchedUsername = helper.checkString(req.body.usernameInput, "username", true);
+      const searchedUser = await userFuncs.getUser(searchedUsername)
+      res.render('adminAccounts', { searchedUsername: searchedUsername, searchedUser: searchedUser ,title:"User Management Panel"});
     } catch (e) {
       if (!e.code) {
         req.session.status = 500;
@@ -164,8 +103,9 @@ router
     }
   })
   .patch(isAdmin, async (req, res) => {
+    let searchedUsername = req.body.searchedUsername;
     let username = req.body.usernameInput;
-    let avatar = req.body.avatarInput;
+    // let avatar = req.body.avatarInput;
     let firstName = req.body.firstNameInput;
     let lastName = req.body.lastNameInput;
     let phone = req.body.phoneInput;
@@ -176,7 +116,11 @@ router
       if (password !== confirmPassword) {
         throw CustomException("Password and confirm password do not match.", true);
       }
-      const searchedUsername = helper.checkString(req.body.searchedUsername, "searched username", true);
+      searchedUsername = helper.checkString(
+        searchedUsername, 
+        "searched username", 
+        true
+      );
       username = helper.checkString(
         username,
         "username",
@@ -197,7 +141,7 @@ router
       email = helper.checkEmail(email, true);
       const set = {
         username: username,
-        avatar: avatar,
+        // avatar: avatar,
         firstName: firstName,
         lastName: lastName,
         phone: phone,
@@ -206,8 +150,9 @@ router
       };
       const updatedUser = await userFuncs.updateUser(searchedUsername, set);
 
+      const researchedUser = await userFuncs.getUser(username);
       req.flash('Update User sucessfully');
-      return res.redirect(`/admin/accounts`); //Jichen: if you use redirect, it would be a get request.
+      return res.render('adminAccounts', { searchedUsername: username, searchedUser: researchedUser ,title:"User Management Panel"});
     } catch (e) {
       if (!e.code) {
         req.session.status = 500;
@@ -235,7 +180,7 @@ router
     }
   })
 
-router
+  router
   .route('/accounts/createNewAccount')
   .get(isAdmin, (req, res) => {
     const code = req.session && req.session.status ? req.session.status : 200;
