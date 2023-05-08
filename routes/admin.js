bf21d60ae8e06193d859da9c85af03c5d7034659
zainzlh <67248490+zainzlh@users.noreply.github.com>
2023-05-08 15:48:11 -0400

    change createHotel from admin to hotel routes

--- routes/admin.js
+++ routes/admin.js
@@ -236,47 +236,4 @@ router
       return res.redirect("/admin/accounts/createNewAccount");
     }
   });
-
-router
-  .route('/createHotel')
-  .get(isAdmin, async (req, res) => {
-    res.render('adminHotel', {title:"Hotel Creating Panel"});
-  })
-  .post(isAdmin, async (req, res) => {
-    try {
-      let hotelInput = req.body;
-      let args = [];
-      args[0] = helper.checkString(hotelInput.nameInput, "hotel name", true);
-      args[1] = helper.checkString(hotelInput.streetInput, "street", true);
-      args[2] = helper.checkString(hotelInput.cityInput, "city", true);
-      args[3] = helper.checkString(hotelInput.stateInput, "state", true);
-      args[4] = helper.checkZip(hotelInput.zipCodeInput, true);
-      args[5] = helper.checkPhone(hotelInput.phoneInput, true);
-      args[6] = helper.checkEmail(hotelInput.emailInput, true);
-      args[7] = hotelInput.picturesInput
-        ? hotelInput.picturesInput.map((web) => helper.checkWebsite(web, true))
-        : [];
-      if (hotelInput.facilitiesInput && Array.isArray(hotelInput.facilitiesInput)) {
-        args[8] = hotelInput.facilitiesInput.map((facility) =>
-          helper.checkString(facility, "facility", true)
-        );
-      } else if (!hotelInput.facilitiesInput) {
-        args[8] = [];
-      } else {
-        throw CustomException("Invalid facilities.", true);
-      }
-      args[9] = [];
-      const addHotelId = await hotelFuncs.addHotel(args);
-      req.flash('Create hotel successfully');
-      res.redirect("/admin/createHotel");
-    } catch (e) {
-      if (!e.code) {
-        req.session.status = 500;
-      } else {
-        req.session.status = e.code;
-      }
-      req.session.errorMessage = e.message;
-      return res.redirect("/admin/createHotel");
-    }
-  })
 export default router;
