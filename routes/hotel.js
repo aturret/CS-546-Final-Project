5de1ce5934f9914f3dcfcf9caace0be61a920be1
zainzlh <67248490+zainzlh@users.noreply.github.com>
2023-05-09 15:48:31 -0400

    multer apply

--- routes/hotel.js
+++ routes/hotel.js
@@ -219,27 +219,21 @@ router
     try {
       let hotelInput = req.body;
       let args = [];
-      args[0] = helper.checkString(hotelInput.nameInput, "hotel name", true);
-      args[1] = helper.checkString(hotelInput.streetInput, "street", true);
-      args[2] = helper.checkString(hotelInput.cityInput, "city", true);
-      args[3] = helper.checkString(hotelInput.stateInput, "state", true);
-      args[4] = helper.checkZip(hotelInput.zipCodeInput, true);
-      args[5] = helper.checkPhone(hotelInput.phoneInput, true);
-      args[6] = helper.checkEmail(hotelInput.emailInput, true);
-      args[7] = hotelInput.hotelPhotoInput
-        ? hotelInput.hotelPhotoInput.map((web) => helper.checkWebsite(web, true))
-        : [];
-      if (hotelInput.facilitiesInput && Array.isArray(hotelInput.facilitiesInput)) {
-        args[8] = hotelInput.facilitiesInput.map((facility) =>
-          helper.checkString(facility, "facility", true)
-        );
-      } else if (!hotelInput.facilitiesInput) {
-        args[8] = [];
-      } else {
-        throw CustomException("Invalid facilities.", true);
-      }
+      const name = helper.checkString(hotelInput.hotelNameInput, "hotel name", true);
+      // const pictures = hotelInput.hotelPhotoInput
+      //   ? hotelInput.hotelPhotoInput.map((web) => helper.checkWebsite(web, true))
+      //   : [];
+      const pictures = [];
+      const facilities = helper.checkString(hotelInput.hotelFacilitiesInput, "facilities", true);
+      const phone = helper.checkPhone(hotelInput.hotelPhoneInput, true);
+      const email = helper.checkEmail(hotelInput.hotelEmailInput, true);
+      const city = helper.checkString(hotelInput.hotelCityInput, "city", true);
+      const street = helper.checkString(hotelInput.hotelStreetInput, "street", true);
+      const state = helper.checkString(hotelInput.hotelStateInput, "state", true);
+      const zipcode = helper.checkZip(hotelInput.hotelZipcodeInput, true);
+      
       args[9] = [];
-      const addHotelId = await hotelFuncs.addHotel(args);
+      const addHotelId = await hotelFuncs.addHotel(name, street, city, state, zipcode, phone, email, pictures, facilities, []);
       req.flash("success", 'Create hotel successfully');
       res.redirect("/admin/createHotel");
     } catch (e) {
@@ -582,13 +576,13 @@ router
   })
   .post(isMgr, upload.array("newPicturesInput", 10), async (req, res, next) => {
     if(req.files.length > 0){
-      req.body.newPictures = req.files.map(file => `http://localhost:3000/public/uploads/${file.filename}`);
+      req.body.newPicturesInput = req.files.map(file => `http://localhost:3000/public/uploads/${file.filename}`);
     }
     next();
   }, async (req, res) => {
     const hotelId = helper.checkId(req.params.hotelId, true);
     try {
-      const pictures = helper.checkArray(req.body.newPictures, 'new roomtype pictures', true);
+      const pictures = helper.checkArray(req.body.newPicturesInput, 'new roomtype pictures', true);
       const roomTypeName = helper.checkString(req.body.newRoomTypeNameInput, "room type name", true);
       // const pictures = req.files.map(file => `http://localhost:3000/public/uploads/${file.filename}`);
       const price = helper.checkPrice(parseFloat(req.body.newPriceInput), true);
